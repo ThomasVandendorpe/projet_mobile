e8519f992e9d53babd249b04be16f5e94a350ad1
Cyprien Eymond Laritaz <cyprien@lambdalab.fr>
2020-02-19 14:48:25 +0100

    Google authentication

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -12,56 +12,71 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  
-  createAccountPage : CreateAccountPage
-  formAdd: { email: string, password: string,  rpassword: string } = { email: "", password: "", rpassword: "" }
-  firstAttempt : boolean = false
-  
-  constructor(private router: Router, private auth : AuthGuardService,private modalController: ModalController) { }
-  
-  
+
+  createAccountPage: CreateAccountPage
+  formAdd: { email: string, password: string, rpassword: string } = { email: "", password: "", rpassword: "" }
+  firstAttempt: boolean = false
+
+  constructor(private router: Router, private auth: AuthGuardService, private modalController: ModalController) { }
+
+
   ngOnInit() {
   }
-  
+
   logForm() {
     console.log(this.formAdd)
     firebase.auth()
-    .signInWithEmailAndPassword(this.formAdd.email, this.formAdd.password)
-    .then(res=>{
-      this.auth.login()
-      this.router.navigate(['/todolist'])
-    },
-    err =>{
-      this.firstAttempt = true
-    }
-    ).catch(function(error) {
-      
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
+      .signInWithEmailAndPassword(this.formAdd.email, this.formAdd.password)
+      .then(res => {
+        this.auth.login()
+        this.router.navigate(['/todolist'])
+      },
+        err => {
+          this.firstAttempt = true
+        }
+      ).catch(function (error) {
+
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
   }
-  
+
   async presentModal() {
     const modal = await this.modalController.create({
       component: CreateAccountPage
     });
     return await modal.present();
   }
-  
+
   fb_auth() {
-    // var provider = new firebase.auth.FacebookAuthProvider();
-    // firebase.auth()
-    // .signInWithRedirect(provider)
-    // .then(result => {
-    //   this.auth.login()
-    //   this.router.navigate(['/todolist'])
-    // }).catch(error => {
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    //   var email = error.email;
-    //   var credential = error.credential;
-    // });
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        this.auth.login()
+        this.router.navigate(['/todolist'])
+      }).catch(error => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+        });
   }
-  
+
+  google_auth() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        this.auth.login();
+        this.router.navigate(['/todolist']);
+      }).catch(error => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+      });
+
+  }
 }
